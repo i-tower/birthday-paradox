e0aa3e45903e3037bbf4b4676d11e61628e029b4
ITower <25020681+i-tower@users.noreply.github.com>
2023-04-21 17:40:14 -0400

    Begin impolementing the main loop

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use generator::*;
 mod generator;
-
+const COHORT_SIZE: usize = 23;
 fn main() {
-    let cohort = Cohort::build_cohort(23);
+    let cohort = Cohort::build(23);
 
     println!("{:?}", cohort);
 
@@ -11,6 +11,24 @@ fn main() {
     } else {
         println!("Same birthday not found");
     }
+
+    let runs = 100;
+    let mut loop_count = 0;
+    let mut match_count = 0;
+    loop {
+        let cohort = Cohort::build(COHORT_SIZE);
+        if cohort.check_for_same() {
+            match_count += 1;
+        }
+        loop_count += 1;
+        if loop_count == runs {
+            break
+        }
+    }
+
+    let result: f64  = (match_count / runs) * 100;
+
+    println!("{}% chance of match", result);
 
 
 }
