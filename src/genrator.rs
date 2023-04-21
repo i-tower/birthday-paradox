f6ee822c7865376d598dba02f4ebc2c43f375be0
ITower <25020681+i-tower@users.noreply.github.com>
2023-04-21 17:25:32 -0400

    Implement checking for same birthdays

--- src/genrator.rs
+++
@@ -1,9 +0,0 @@
-use chrono::prelude::*;
-use rand::{thread_rng, Rng};
-
-fn gen_dates(sample_size: usize, runs: usize) -> Vec<NaiveDate> {
-
-    
-
-    todo!();
-}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-mod genrator;
+use generator::*;
+mod generator;
 
 fn main() {
-     
-     
+    let cohort = Cohort::build_cohort(23);
+
+    println!("{:?}", cohort);
+
+    if cohort.check_for_same() {
+        println!("Someone has the same birthday!!")
+    } else {
+        println!("Same birthday not found");
+    }
+
+
 }
