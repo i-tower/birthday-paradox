ecedaff02401b248932d14202fad5df0feb01404
ITower <25020681+i-tower@users.noreply.github.com>
2023-04-22 16:32:43 -0400

    Cleanup and add comments. It works!

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use std::time::Instant;
 mod generator;
 const COHORT_SIZE: usize = 23;
 const NUMBER_OF_RUNS: i32 = 1000000;
+
+//TODO: Implement command line parsing for COHORT SIZE and NUMBER OF RUNS
+
 fn main() {
     let now = Instant::now();
     let mut loop_count = 0;
@@ -15,14 +18,12 @@ fn main() {
         }
         loop_count += 1;
         if loop_count == NUMBER_OF_RUNS {
-            break
+            break;
         }
     }
     let elapsed = now.elapsed();
-    
-    let result: f64  = (match_count as f64 / NUMBER_OF_RUNS as f64) * 100.0;
+
+    let result: f64 = (match_count as f64 / NUMBER_OF_RUNS as f64) * 100.0;
     println!("Simulation took {:.2?}", elapsed);
     println!("{:.2}% chance of match", result);
-
-
 }
